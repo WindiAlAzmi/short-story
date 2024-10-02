0476511eaaeee74acb4a8a6282779f315617adfe
windi al azmi <windi.azmy@gmail.com>
2024-10-02 16:09:23 +0700

    update first load

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import LoadingComponent from "./LoadingComponent";
+// import LoadingComponent from "./LoadingComponent";
 
 export default function Layout() {
 
@@ -10,7 +10,7 @@ export default function Layout() {
       <Header/>
       <Outlet/>
       <Footer/>
-      <LoadingComponent />
+      {/* <LoadingComponent /> */}
     </div>
   )
 }
--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -1,20 +1,19 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setLoading } from "../features/generalSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setLoading } from "../features/generalSlice";
 
 export default function LoadingComponent() {
-    const loadData = useSelector((state) => state.general);
-    const dispatch = useDispatch();
+    // const loadData = useSelector((state) => state.general);
+    // const dispatch = useDispatch();
 
    useEffect(() => {
-     setTimeout(() => {
-       dispatch(setLoading(false))
-     }, 3000);
-  }, [loadData])
+    //  setTimeout(() => {
+    //    dispatch(setLoading(false))
+    //  }, 3000);
+  }, [])
 
  return (
-   loadData?.loading && (
+   (
      <div className="absolute z-[999] pt-[320px] bg-black/20 w-full flex flex-row md:items-center items-start justify-center mx-auto inset-0">
        <img
          src="/assets/icons/loading-blush.png"
